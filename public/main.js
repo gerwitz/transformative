7ca106299241d1f76e49b82f4b85dce7ac0c9de5
Hans Gerwitz <hans@gerwitz.com>
2018-03-18 15:50:26 +0100

    more lobotomizing

--- public/main.js
+++
@@ -1,8 +0,0 @@
-// register sw script in supporting browsers
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(() => {
-    console.log('Service Worker registered successfully.');
-  }).catch(error => {
-    console.log('Service Worker registration failed:', error);
-  });
-}
